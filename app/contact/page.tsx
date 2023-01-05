49fd0b435acb564e7adbef9c2bbeabd24143e955
vkotu <vkotu@paypal.com>
2023-01-04 21:14:18 -0800

    first commit

---
+++ app/contact/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default  function Contact() {
+    return (
+       <div>
+        <h1>Contact</h1>
+       <Link href="/">home</Link>
+       </div>
+    )
+}
